8b474710e657a0e3d45f318854ff2bbfab2dd47a
Smriti Porwal <smriti941144@gmail.com>
2023-01-15 18:55:21 +0530

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HeaderComponent from "./Components/header-component";
 import BodyComponent from "./Components/body-component";
 import NavPanel from "./Components/nav-panel";
 import { useSelector } from "react-redux";
+import  FootetComponent from "./Components/footer-component";
 
 function App() {
   const toggleSlice = useSelector((state) => state.navToggleSlice.toggleNav);
@@ -13,6 +14,9 @@ function App() {
       <div className="grid grid-flow-col grid-cols-12">
         {toggleSlice && <NavPanel />}
         <BodyComponent />
+      </div>
+      <div>
+      <FootetComponent />
       </div>
     </>
   );
---
+++ src/Components/footer-component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FootetComponent = () => {
+  return (
+    <div className='m-2 p-2 bg-gray-200 text-center font-bold'>
+      <h2>Developed by Smriti !!</h2>
+    </div>
+  )
+}
+
+export default FootetComponent;
